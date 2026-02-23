1b45f9b097364fb0f1d8b4bffefddddbe980cded
Pedro-DiasZ <pedrohenriquediasmoreira1@gmail.com>
2026-02-23 12:25:56 +0000

    Adicionando CORS e fixando bugs

--- back/main.py
+++ back/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from schemas import PraySchema
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return {"message": "API is running"}
-
 
 @app.post("/Prayer")
 def create_pray(pray: PraySchema):
