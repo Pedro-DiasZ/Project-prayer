19bc8700146b124346b0daffb91133c595ea5866
Pedro-DiasZ <pedrohenriquediasmoreira1@gmail.com>
2026-02-23 15:24:57 -0300

    Update main.py

--- back/main.py
+++ back/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from schemas import PraySchema
+from database import engine
+import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
