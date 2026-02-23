1d7e313819ec70b24100dc76256cb1f195f8c45f
Pedro-DiasZ <pedrohenriquediasmoreira1@gmail.com>
2026-02-23 15:34:50 -0300

    Update main.py

--- back/main.py
+++ back/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 from schemas import PraySchema
-from database import engine
+from database import engine, SessionLocal
 import models
 
 models.Base.metadata.create_all(bind=engine)
@@ -16,10 +17,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 async def root():
     return {"message": "API is running"}
 
 @app.post("/prayer")
-def create_pray(pray: PraySchema):
-    return pray
+def create_pray(pray: PraySchema, db: Session = Depends(get_db)):
+    db_pray = models.Prayer(name=pray.name, prayer=pray.prayer)
+    db.add(db_pray)
+    db.commit()
+    db.refresh(db_pray)
+    return db_pray
